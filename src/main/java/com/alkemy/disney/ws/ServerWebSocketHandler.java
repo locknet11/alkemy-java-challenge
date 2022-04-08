bf062f2f9b9d65b4d00263a3c25c03f218d89dfc
locknet11 <locknet41@protonmail.com>
2022-04-08 13:35:01 -0300

    Update ServerWebSocketHandler.java

--- src/main/java/com/alkemy/disney/ws/ServerWebSocketHandler.java
+++ src/main/java/com/alkemy/disney/ws/ServerWebSocketHandler.java
@@ -1,6 +1,9 @@
 package com.alkemy.disney.ws;
 
-import org.springframework.web.socket.CloseStatus;
+import java.util.Map;
+
+import org.springframework.boot.json.JsonParser;
+import org.springframework.boot.json.JsonParserFactory;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -11,9 +14,29 @@ public class ServerWebSocketHandler extends TextWebSocketHandler {
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 	    String request = message.getPayload();
-	    String response = String.format("MUTHER from server to '%s'", HtmlUtils.htmlEscape(request));
-	    session.sendMessage(new TextMessage(response));
-	    session.close(CloseStatus.NORMAL);
+	    
+	    try {
+	    	
+	    	JsonParser jp = JsonParserFactory.getJsonParser();
+	    	
+	    	Map<String, Object> map = jp.parseMap(request);
+	    	
+	    	Object jwt = map.get("token");
+	    	
+	    	String response = String.format("Your token is: %s", jwt);
+	    	
+		    session.sendMessage(new TextMessage(response));
+		    
+		    Object close = map.get("close");
+		    
+		    if(Boolean.parseBoolean(close.toString())) {
+		    	session.close();
+		    }
+	    	
+	    }catch(Exception e) {
+	    	System.out.println("Error parsing JSON");
+	    }
+
 	}
 	
 
